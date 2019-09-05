b0255f2a9b461ef1e8490a8f3ce2982cc779c5ad
guoqingyao <stepbystepto@163.com>
2019-09-05 16:57:36 +0800

    app-init

---
+++ app/src/pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="userinfo">
+      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
+      <div class="userinfo-nickname">
+       {{userInfo.nickName}}
+      </div>
+    </div>
+    <div  class="btn">
+      <i-button>进入小程序</i-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      userInfo: {
+        nickName: 'ccnews',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+  methods: {
+    bindViewTap () {
+      const url = '../newsList/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.userinfo-avatar {
+  width: 180rpx;
+  height: 180rpx;
+  margin-top: 200rpx;  
+}
+.btn {
+  margin: 0 auto;
+  margin-top: 180rpx;
+  width: 500rpx;
+}
+</style>
---
+++ app/src/pages/newsList/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div @click="clickHandle">
+
+    <div class="userinfo">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
+      <div class="userinfo-nickname">
+       {{userInfo.nickName}}
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+  methods: {
+    bindViewTap () {
+      const url = '../list/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
